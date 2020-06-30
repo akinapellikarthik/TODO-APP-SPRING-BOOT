e55d07f9d3f570e8b62e8a7c8b598a6be277e0fd
AKINAPELLI KARTHIK <36639366+akinapellikarthik@users.noreply.github.com>
2020-06-30 12:08:14 +0530

    modified pom and todoapp

--- src/main/java/com/karthik/todo/todoapp/TodoappApplication.java
+++ src/main/java/com/karthik/todo/todoapp/TodoappApplication.java
@@ -2,19 +2,14 @@ package com.karthik.todo.todoapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TodoappApplication extends SpringBootServletInitializer{
+public class TodoappApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoappApplication.class, args);
 	}
 	
-	@Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(TodoappApplication.class);
-    }
+
 
 }
